6f5d919ea616ffff4ce3e8c61ac7d89136061d87
leidenit <leidenit@gmail.com>
2014-05-15 10:12:28 +0300

    Create Life algoritm v1.3(only function.cpp
    Вызов: lifrmain(startmatr,ngen,mmax,nmax) (можно заменить функцию vis на функцию прорисовки)

---
+++ Life algoritm v1.3(only function.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+const int kmax=20,mmax=20;
+using namespace std;
+
+void transform(int mask[][mmax], char vis[][mmax],int c,int i,int l) //Изменяет числовую матрицу в зависимости от условий
+{
+    if(i>0 && l>0)mask[i-1][l-1]+=c;
+    if(l>0)mask[i][l-1]+=c;
+    if(l>0 && i<kmax-1)mask[i+1][l-1]+=c;
+    if(i<kmax-1)mask[i+1][l]+=c;
+    if(l<mmax-1 && i<kmax-1)mask[i+1][l+1]+=c;
+    if(i<kmax-1)mask[i][l+1]+=c;
+    if(i>0 && i<kmax-1)mask[i-1][l+1]+=c;
+    if(i>0)mask[i-1][l]+=c;
+}
+
+void viv(int mask[][mmax], char vis[][mmax]) //Типо вывод
+{
+    for(int i=0; i<kmax;i++)
+     {
+        for(int l=0; l<mmax;l++)
+        {
+           cout<< mask[i][l];
+        }
+        cout << endl;
+    }
+     cout<<endl;
+
+    for(int i=0; i<kmax;i++)
+     {
+        for(int l=0; l<mmax;l++)
+        {
+           printf("%c",vis[i][l]);
+        }
+        cout << endl;
+    }
+  cout<<endl;
+  cout<<endl;
+}
+
+void lifemain(char vis[][mmax],int ngen,int mmax,int kmax)  // Главная функция подсчёта vis-основное поле, ngen-кол. поколений
+{                                                           // nmax, kmax - размер матрицы
+ int mask[kmax][mmax];
+
+ for(int i=0; i<kmax;i++)
+     for(int l=0; l<mmax;l++)
+     {
+         mask[i][l]=0;
+     }
+ for(int i=0; i<kmax;i++)
+     for(int l=0; l<mmax;l++)
+     {
+         if(vis[i][l]=='*')
+         {
+             transform(mask,vis,1,i,l);
+         }
+     }
+
+
+ for(int p=0;p<ngen;p++){
+     for(int i=0; i<kmax;i++){
+         for(int l=0; l<mmax;l++)
+         {
+             if(mask[i][l]==3 && vis[i][l]=='_')
+             {
+                 vis[i][l]='*';
+                 transform(mask,vis,1,i,l);
+             }
+             else if(mask[i][l]>3 && vis[i][l]=='*')
+                {
+                 vis[i][l]='_';
+                 transform(mask,vis,-1,i,l);
+             }
+             else if(mask[i][l]<2 && vis[i][l]=='*')
+             {
+                 vis[i][l]='_';
+                 transform(mask,vis,-1,i,l);
+
+             }
+         }
+     }
+     viv(mask,vis);
+  }
+}
